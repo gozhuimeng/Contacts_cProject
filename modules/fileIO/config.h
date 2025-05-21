17384f097707200a0e587e0960c56547b494b1ec
meng <gozhuimeng@outlook.com>
2025-05-21 17:33:43 +0800

    09.250521 添加配置文件config.h，用于指定文件路径

---
+++ modules/fileIO/config.h
@@ -0,0 +1,11 @@
+//
+// Created by meng on 25-5-21.
+//
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define DATA_FILE_NAME "../data/data.csv" // 数据文件名
+#define DATA_FILE_NAME_OUT "../data/data_temp.csv" // 缓存文件名
+
+#endif //CONFIG_H
