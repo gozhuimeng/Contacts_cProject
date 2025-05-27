61fb1f1375e30efb0369541b042dfaedc37d24aa
meng <gozhuimeng@outlook.com>
2025-05-27 17:36:01 +0800

    16.250527 创建主函数函数库

---
+++ modules/func.c
@@ -0,0 +1,22 @@
+//
+// Created by meng on 25-5-27.
+// DOC 主程序函数
+//
+
+#include <stdio.h>
+
+#include "func.h"
+
+void print_menu() {
+    printf("========请选择进行的操作========\n");
+    printf("\t1. 导入数据\n");
+    printf("\t2. 插入数据\n");
+    printf("\t3. 浏览数据\n");
+    printf("\t4. 查询数据\n");
+    printf("\t5. 修改数据\n");
+    printf("\t6. 删除数据\n");
+    printf("\t7. 保存数据\n");
+    printf("\t8. 导出数据\n");
+    printf("\t0. 退出\n");
+    printf("请输入操作序号: ");
+}
---
+++ modules/func.h
@@ -0,0 +1,11 @@
+//
+// Created by meng on 25-5-27.
+// DOC 主程序函数
+//
+
+#ifndef FUNC_H
+#define FUNC_H
+
+void print_menu();  // 打印菜单
+
+#endif //FUNC_H
