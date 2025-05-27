833a3239fe10794fcc77624e9239bf1ed9bf47ed
meng <gozhuimeng@outlook.com>
2025-05-27 17:19:15 +0800

    14.250527 添加Grid比对函数

---
+++ modules/tool.c
@@ -0,0 +1,39 @@
+//
+// Created by meng on 25-5-23.
+// DOC 工具函数
+//
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "fileIO/dataFormat.h"
+#include "tool.h"
+
+#include <string.h>
+
+bool check_grid_equal(Grid *grid1, Grid *grid2) {
+    if (grid1 == NULL || grid2 == NULL) {
+        return false;
+    }
+    if (grid1->len != grid2->len) {
+        return false;
+    }
+    for (int i = 0; i < grid1->len; i++) {
+        Row *row1 = grid1->data[i];
+        Row *row2 = grid2->data[i];
+        if (row1->len != row2->len) {
+            return false;
+        }
+        for (int j = 0; j < row1->len; j++) {
+            Node *node1 = row1->data[j];
+            Node *node2 = row2->data[j];
+            if (node1->len != node2->len) {
+                return false;
+            }
+            if (strcmp(node1->str, node2->str)) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
---
+++ modules/tool.h
@@ -0,0 +1,15 @@
+//
+// Created by meng on 25-5-23.
+// DOC 工具函数
+//
+
+#ifndef TOOL_H
+#define TOOL_H
+
+#include<stdbool.h>
+
+#include "fileIO/dataFormat.h"
+
+bool check_grid_equal(Grid *grid1, Grid *grid2);
+
+#endif //TOOL_H
